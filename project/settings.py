161b854d2645a988fb696e6c71978fc8fb5316b9
zhangzheng <zzgigi2003@163.com>
2017-08-05 22:59:24 +0800

    rename app to api

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'app',
+    'api',
     'rest_framework',
 ]
 
--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from rest_framework import routers
-from app import views
+from api import views
 
 router = routers.DefaultRouter()
 router.register(r'categorys', views.CategoryViewSet)
