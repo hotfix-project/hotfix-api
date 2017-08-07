9de0aaca3455e21bd2d63f7a5c7ee9f77f2b7f20
zhangzheng <zzgigi2003@163.com>
2017-08-07 04:34:44 +0000

    name set to unique

--- api/models.py
+++ api/models.py
@@ -6,14 +6,14 @@ import datetime
 
 class System(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=1024, null=False)
+    name = models.CharField(max_length=64, null=False, unique=True)
 
     def __str__(self):  
         return self.name
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=1024, null=False)
+    name = models.CharField(max_length=64, null=False, unique=True)
   
     def __str__(self):  
         return self.name
@@ -22,7 +22,7 @@ class App(models.Model):
     id = models.AutoField(primary_key=True)
     category_id = models.ForeignKey(Category)  
     system_id = models.ForeignKey(System)  
-    name = models.CharField(max_length=1024, null=False)
+    name = models.CharField(max_length=64, null=False, unique=True)
     key = models.CharField(max_length=1024, null=False)
     secret = models.CharField(max_length=1024, null=False)
     rsa = models.TextField(null=False)
@@ -34,7 +34,7 @@ class App(models.Model):
 class Version(models.Model):
     id = models.AutoField(primary_key=True)
     app_id = models.ForeignKey(App)  
-    name = models.CharField(max_length=1024, null=False)
+    name = models.CharField(max_length=64, null=False, unique=True)
     create_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):  
