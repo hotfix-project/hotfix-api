ddd0e952f63a64a275868f8528c894c4b170acbe
zhangzheng <zzgigi2003@163.com>
2017-08-16 10:48:14 +0800

    rename url: hotfix to api

--- project/urls.py
+++ project/urls.py
@@ -35,7 +35,7 @@ schema_view = get_swagger_view(title='Hotfix API')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hotfix/', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include_docs_urls(title='Hotfix API')),
     url(r'^$', schema_view),
