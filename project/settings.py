0db61bd548b65480cd4e1e40a676ad33516e3c3b
zhangzheng <zhangzheng@qianbao.com>
2017-08-16 12:18:46 +0800

    default auth: permissions.IsAuthenticated

--- project/settings.py
+++ project/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
